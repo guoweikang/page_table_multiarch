159071b65c880479e007accd757cea9f64ddbb38
Weikang Guo <guoweikang@kylinos.cn>
2025-12-26 18:27:08 +0800

    test
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- page_table_multiarch/src/arch/aarch64.rs
+++ page_table_multiarch/src/arch/aarch64.rs
@@ -25,14 +25,13 @@ impl PagingMetaData for A64PagingMetaData {
     fn flush_tlb(vaddr: Option<VirtAddr>) {
         // make sure all previous page table writes are visible
         unsafe {
-            asm!("dsb ishst");
             if let Some(vaddr) = vaddr {
                 // TLB Invalidate by VA, All ASID, EL1, Inner Shareable
                 const VA_MASK: usize = (1 << 44) - 1; // VA[55:12] => bits[43:0]
-                asm!("tlbi vaae1is, {}; dsb sy; isb", in(reg) ((vaddr.as_usize() >> 12) & VA_MASK))
+                asm!("dsb ishst, tlbi vaae1is, {}; dsb ish; isb", in(reg) ((vaddr.as_usize() >> 12) & VA_MASK))
             } else {
                 // TLB Invalidate by VMID, All at stage 1, EL1
-                asm!("tlbi vmalle1is; dsb sy; isb")
+                asm!("dsb ishst, tlbi vmalle1is; dsb ish; isb")
             }
         }
     }
